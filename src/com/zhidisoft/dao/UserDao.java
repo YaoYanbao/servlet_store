db8a9737960bdaf1673d40cedcf1c151463ea109
YaoYanbao <1402035404@qq.com>
2018-01-22 12:37:51 +0800

    代码架子编写

---
+++ src/com/zhidisoft/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.zhidisoft.dao;
+
+/**
+ * @Author: Yao YanBao
+ * @Description:
+ * @Date: Create in 2018-1-20 16:41
+ */
+public interface UserDao {
+
+}
---
+++ src/com/zhidisoft/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.zhidisoft.service.impl;
+
+/**
+ * @Author: Yao YanBao
+ * @Description:
+ * @Date: Create in 2018-1-20 16:44
+ */
+public class UserServiceImpl {
+
+}
